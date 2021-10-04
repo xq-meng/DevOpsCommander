3fb285e27e30ff3693bfd20d47efea497eba4f22
xqmeng <t-xiameng@microsoft.com>
2021-10-04 15:00:52 +0800

    Init repo, first commit

---
+++ DevOpsCommander/DevOpsCommander.cs
@@ -0,0 +1,85 @@
+﻿using Azure.Storage.Queues;
+using MessageQueue;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DevOpsCommander
+{
+    internal class DevOpsCommander
+    {
+        static void MessageAnalysis(string theMessage, out string prefix, out string message)
+        {
+            if(string.IsNullOrEmpty(theMessage) || theMessage[0] != '[')
+            {
+                prefix = "";
+                message = theMessage;
+                return;
+            }
+            int prefixIndex = theMessage.IndexOf(']');
+            prefix = theMessage.Substring(0, prefixIndex + 1);
+            message = theMessage[(prefixIndex + 1)..];
+        }
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Azure Queue Storage client library v12.");
+
+            string ConnectionString, QueueName;
+            // Read json
+            Dictionary<string, string> messageQueueConfig;
+            try
+            {
+                string messageQueueConfigText = System.IO.File.ReadAllText("AzureStorgeQueue.json");
+                messageQueueConfig = JsonConvert.DeserializeObject<Dictionary<string, string>>(messageQueueConfigText);
+                ConnectionString = messageQueueConfig["ConnectionString"];
+                QueueName = messageQueueConfig["QueueName"];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            if(args.Length != 1)
+            {
+                return;
+            }
+
+            string prefix = "[AZURECLOUD]";
+            string toSend = prefix + args[0];
+
+            QueueClient theQueue = await MQOperation.QueueConnection(ConnectionString, QueueName);
+            await MQOperation.InsertMessageAsync(theQueue, toSend);
+
+            // Wait for end signal.
+            DateTime timeSpanStart = DateTime.Now;
+            while(true)
+            {
+                DateTime timeSpanEnd = DateTime.Now;
+                TimeSpan timeSpan = timeSpanEnd - timeSpanStart; 
+                if(timeSpan.TotalMinutes > 30)
+                {
+                    Console.WriteLine("DevOps Commander time out. ");
+                    break;
+                }
+                string topMessage = await MQOperation.PeekTopMessageAsync(theQueue);
+                MessageAnalysis(topMessage, out string tempPrefix, out string tempMessage);
+                if(tempPrefix == "[WORKING_MACHINE]")
+                {
+                    timeSpanStart = DateTime.Now;
+                    topMessage = await MQOperation.RetrieveNextMessageAsync(theQueue);
+                    Console.WriteLine(topMessage);
+                    if(tempMessage == "[END_SIGNAL]")
+                    {
+                        Console.WriteLine("Retrieved the end signal. ");
+                        break;
+                    }
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ DevOpsCommander/MQOperation.cs
@@ -0,0 +1,79 @@
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace MessageQueue
+{
+    public class MQOperation
+    {
+        public static Task<QueueClient> QueueConnection(string ConnectionString, string QueueName)
+        {
+            QueueClient queueClient = new QueueClient(ConnectionString, QueueName);
+
+            return Task.FromResult(queueClient);
+        }
+
+        public static async Task InsertMessageAsync(QueueClient theQueue, string newMessage)
+        {
+            if(null != await theQueue.CreateIfNotExistsAsync())
+            {
+                Console.WriteLine("The queue was created.");
+            }
+
+            await theQueue.SendMessageAsync(newMessage);
+        }
+
+        public static async Task<string> RetrieveNextMessageAsync(QueueClient theQueue)
+        {
+            if(await theQueue.ExistsAsync())
+            {
+                QueueProperties properties = await theQueue.GetPropertiesAsync();
+                
+                if(properties.ApproximateMessagesCount > 0)
+                {
+                    QueueMessage[] retrievedMessage = await theQueue.ReceiveMessagesAsync(1);
+                    if(retrievedMessage.Length == 0)
+                    {
+                        return "";
+                    }
+                    string theMessage = retrievedMessage[0].Body.ToString();
+                    await theQueue.DeleteMessageAsync(retrievedMessage[0].MessageId, retrievedMessage[0].PopReceipt);
+                    return theMessage;
+                }
+
+                else
+                {
+                    return "";
+                }
+            }
+
+            return "";
+        }
+
+        public static async Task<string> PeekTopMessageAsync(QueueClient theQueue)
+        {
+            if(await theQueue.ExistsAsync())
+            {
+                QueueProperties properties = await theQueue.GetPropertiesAsync();
+
+                if(properties.ApproximateMessagesCount > 0)
+                {
+                    PeekedMessage[] retrievedMessage = await theQueue.PeekMessagesAsync(1);
+                    if(retrievedMessage.Length == 0)
+                    {
+                        return "";
+                    }
+                    string theMessage = retrievedMessage[0].Body.ToString();
+                    return theMessage;
+                }
+                else
+                {
+                    return "";
+                }
+            }
+            return "";
+        }
+
+    }
+}
